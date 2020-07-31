9738423369cb9908105641d67d68e093ab6009d1
Jan Andrade <cloud.andrade16@gmail.com>
2020-07-31 12:53:46 -0300

    pequena mudança de script

--- src/config/db/scripts/meetup_mysql_create.sql
+++ src/config/db/scripts/meetup_mysql_create.sql
@@ -86,13 +86,3 @@ CREATE TABLE `consultor` (
 	`usuario_id` bigint NOT NULL,
 	PRIMARY KEY (`id`)
 );
-
---insert into medico (nome, email, senha) values ('Roberto Miranda', 'admin@admin.com','$2a$08$IlSG4lrXtDfWV5nprqvEMOS8upzN3e8THUR5EKKbnbNGU/5KSDpt2');
---insert into endereco (logradouro, numero, bairro, cidade) values ('rua das margaridas', 36, 'vale do limoreiro', 'salvador');
---insert into paciente (nome, idade, telefone, endereco_id, email, senha ) values ('João Claudio Andrade', 28, 71986712633, 1, 'claudio@gmail.com', '1234567');
---insert into endereco (logradouro, numero, bairro, cidade) values ('rua jardim das margaridas', 33, 'itapoã', 'salvador');
---insert into paciente (nome, idade, telefone, endereco_id, email, senha ) values ('Joana Simões', 36, 71958623458, 2, 'joanasims@gmail.com', '1234567');
---insert into endereco (logradouro, numero, bairro, cidade) values ('travessa da taboca', 11, 'vale dos rios', 'salvador');
---insert into paciente (nome, idade, telefone, endereco_id, email, senha ) values ('Juliano Silva', 48, 71945782134, 3, 'julianoss@gmail.com', '1234567');
---insert into endereco (logradouro, numero, bairro, cidade) values ('av brasiliana', 142, 'cabula', 'salvador');
---insert into paciente (nome, idade, telefone, endereco_id, email, senha ) values ('Alex Souza', 50, 7198671263, 4, 'alexus@gmail.com', '1234567');
